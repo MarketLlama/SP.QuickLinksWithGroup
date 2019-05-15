846ee7c60fc29a7ef5527d0a9024b29ca0fc0651
Mark Williams <mark@Marks-MacBook-Pro.local>
2019-05-15 13:02:11 +0100

    Intial Commit

---
+++ src/webparts/quicklinksWithPermissions/components/IQuicklinksWithPermissionsProps.ts
@@ -0,0 +1,3 @@
+export interface IQuicklinksWithPermissionsProps {
+  description: string;
+}
---
+++ src/webparts/quicklinksWithPermissions/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IQuicklinksWithPermissionsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'QuicklinksWithPermissionsWebPartStrings' {
+  const strings: IQuicklinksWithPermissionsWebPartStrings;
+  export = strings;
+}
